59dc57d09c4a51cac0a2f42c46e8d81af039a0f3
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2025-01-21 15:20:05 +1300

    Update shared.rb

--- shared.rb
+++ shared.rb
@@ -13,7 +13,6 @@ def init_pdf
     italic: "C:\Users\reube\Fonts\Futura\Futura Heavy Italic font.ttf",
     bold: "C:\Users\reube\Fonts\Futura\Futura Bold font.ttf",
     condensed: "C:\Users\reube\Fonts\Futura\futura medium condensed bt.ttf"
-    :
   })
   #pdf.font(FONTS.keys.first)
   pdf.stroke_color MEDIUM_COLOR
