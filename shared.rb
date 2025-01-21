2965dcd29504ff5a279a91e488d4f073fd2294c1
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2025-01-21 15:32:36 +1300

    Update shared.rb

--- shared.rb
+++ shared.rb
@@ -11,9 +11,9 @@ def init_pdf
   pdf = Prawn::Document.new(margin: RIGHT_PAGE_MARGINS, print_scaling: :none)
   pdf.font_families.update( "Futura" => {
     :normal => "C:\Users\reube\Fonts\Futura\Futura.TTF",
-    :italic: "C:\Users\reube\Fonts\Futura\Futura Heavy Italic font.ttf",
-    :bold: "C:\Users\reube\Fonts\Futura\Futura Bold font.ttf",
-    :condensed: "C:\Users\reube\Fonts\Futura\futura medium condensed bt.ttf"
+    :italic => "C:\Users\reube\Fonts\Futura\Futura Heavy Italic font.ttf",
+    :bold => "C:\Users\reube\Fonts\Futura\Futura Bold font.ttf",
+    :condensed => "C:\Users\reube\Fonts\Futura\futura medium condensed bt.ttf"
   })
   # pdf.font(FONTS.keys.first)
   pdf.font("Futura")
